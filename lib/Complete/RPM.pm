f3e2fdcf0138f1381d3114839d357b5f9960a691
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-16 19:05:52 +0700

    Initial commit

---
+++ lib/Complete/RPM.pm
@@ -0,0 +1,41 @@
+package Complete::RPM;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Complete::Setting;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+                       complete_rpm
+               );
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Completion routines related to RPM packages',
+};
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage .+
+
+=head1 DESCRIPTION
+
+B<NAME GRAB. NOT YET IMPLEMENTED.>
+
+
+=head1 SEE ALSO
+
+L<Complete>
+
+L<Complete::Dpkg>
+
+Other C<Complete::*> modules.
